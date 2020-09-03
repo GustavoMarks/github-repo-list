086bc5fb37b18b8b3e7d4ef69c9fe94a40a0ba1f
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-02 21:21:50 -0300

    feat(api): rota de listagem de repositórios do GitHub

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const Router = require('./controllers/Router');
 
 //Configurando aplicação
 const app = express();
@@ -13,5 +14,6 @@ const corsOptions = {
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use(Router);
 
 module.exports = app;
