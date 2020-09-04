865964b9610877b8c5267e627567d86f1bb3fe07
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-04 11:47:58 -0300

    styel(client): tela de carregamento

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Background from '../../assets/simple-codelines.svg';
+import './style.css';
 
-export default function Loading(){
-  return <main><h1> Carregando... </h1></main>
+export default function Loading() {
+  return <main id="load-content" style={{ backgroundImage: `url(${Background})` }}>
+    <h1> carregando... </h1>
+  </main>
 }
