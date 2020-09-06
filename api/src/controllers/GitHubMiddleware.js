3a6635ef3776b8b9b775191e13f045eece505598
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-06 15:04:07 -0300

    fix(api): paginação na requisição e na resposta, issue #6

--- api/src/controllers/GitHubMiddleware.js
+++ api/src/controllers/GitHubMiddleware.js
@@ -7,6 +7,7 @@ const token = process.env.GITHUB_TOKEN == 'false' ? false : process.env.GITHUB_T
 module.exports = {
   async index(req, res) {
     const { username } = req.params;
+    const { page } = req.query;
 
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`[*] Uma requisição à uma lista de repositórios de ${username} foi feita por: ${ip}`);
@@ -14,6 +15,7 @@ module.exports = {
     axios({
       method: 'GET',
       url: `https://api.github.com/users/${username}/repos`,
+      params: page ? { page }: null,
       headers: {
         'Authorization': token ? `token ${token}` : null
       }
@@ -21,7 +23,18 @@ module.exports = {
       .then(response => {
         const limit = response.headers['x-ratelimit-limit'];
         const used = response.headers['x-ratelimit-used'];
-        
+        const link = response.headers['link'];
+
+        if (link) {
+          try {
+            const lastPage = link.split('>;')[1].split('page=')[1]
+            res.header('X-Last-Page', parseInt(lastPage));
+
+          } catch {
+            console.log('[*] Falha ao tentar acessar total de páginas...');
+          }
+        }
+
         console.log(`[*] Requisição realizada com sucesso (${used}/${limit})`)
         return res.send(response.data);
 
@@ -30,7 +43,7 @@ module.exports = {
         const { status } = error.response;
         console.log(`[*] Requisição falhou (${status})`);
 
-        if(status == 404) return res.status(404).send({
+        if (status == 404) return res.status(404).send({
           error,
           msg: 'Este usuername não possui cadastro...'
         });
