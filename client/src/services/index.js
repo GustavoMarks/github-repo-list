ccc6fba3aedc6918c53a529092f3e4d81ee39982
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-07 13:39:44 -0300

    fix(client): removendo ambiguidade de chamada a api

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? `http://localhost:3333/` : (process.env.API || `http://localhost:3333/`);
+const baseURL = process.env.NODE_ENV === 'development' ? `http://localhost:3333/` : process.env.API;
 const api = axios.create({ baseURL });
 
 export default api;
