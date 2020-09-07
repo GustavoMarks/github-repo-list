8cab062d6bb827611988551b10067dd3e39f2d21
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-06 22:03:45 -0300

    build(client): configurações de deploy react

---
+++ client/build/precache-manifest.2f971556ce25826ccf3b6d7f4e2c35fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73700bd666b92602f679e823658ee6be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06217a7192f50df1f208",
+    "url": "/static/css/main.b17e0126.chunk.css"
+  },
+  {
+    "revision": "c6cacb1ef6659143160e",
+    "url": "/static/js/2.88b091cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88b091cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06217a7192f50df1f208",
+    "url": "/static/js/main.9b607bec.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9ca55a886c57aaff3e45a331370a1e4a",
+    "url": "/static/media/simple-codelines.9ca55a88.svg"
+  }
+]);
