5fc9391cd8fcc185db67784dc13fc4a19720acf0
GustavoM7 <gustavosmarquesf@gmail.com>
2020-09-13 23:13:34 -0300

    refactor(api): removendo sintaxe then catch do controller github

--- api/src/controllers/GitHubMiddleware.js
+++ api/src/controllers/GitHubMiddleware.js
@@ -12,59 +12,60 @@ module.exports = {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`[*] Uma requisição à uma lista de repositórios de ${username} foi feita por: ${ip}`);
 
-    axios({
-      method: 'GET',
-      url: `https://api.github.com/users/${username}/repos`,
-      params: page ? { page } : null,
-      headers: {
-        'Authorization': token ? `token ${token}` : null
-      }
-    })
-      .then(response => {
-        const limit = response.headers['x-ratelimit-limit'];
-        const used = response.headers['x-ratelimit-used'];
-        const link = response.headers['link'];
-
-        if (link) {
-          try {
-            const links = link.split(',');
-            const lastLink = links.find(element => element.includes('rel="last"'));
-            if (lastLink) {
-              const lastPage = lastLink.split('>;')[0].split('?page=')[1];
-              res.header('X-Last-Page', parseInt(lastPage));
-
-            } else {
-              // Caso a requisição seja feita na última pagina, não há link de referência
-              const prevLink = links.find(element => element.includes('rel="prev"'));
-              const lastPage = prevLink.split('>;')[0].split('?page=')[1];
-              res.header('X-Last-Page', parseInt(lastPage) + 1);
-
-            }
-
-          } catch {
-            console.log('[*] Falha ao tentar acessar total de páginas...');
-          }
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `https://api.github.com/users/${username}/repos`,
+        params: page ? { page } : null,
+        headers: {
+          'Authorization': token ? `token ${token}` : null
         }
+      });
 
-        console.log(`[*] Requisição realizada com sucesso (${used}/${limit})`)
-        return res.send(response.data);
+      const limit = response.headers['x-ratelimit-limit'];
+      const used = response.headers['x-ratelimit-used'];
+      const link = response.headers['link'];
 
-      })
-      .catch(error => {
-        const { status } = error.response;
-        console.log(`[*] Requisição falhou (${status})`);
+      if (link) {
+        try {
+          const links = link.split(',');
+          const lastLink = links.find(element => element.includes('rel="last"'));
+          if (lastLink) {
+            const lastPage = lastLink.split('>;')[0].split('?page=')[1];
+            res.header('X-Last-Page', parseInt(lastPage));
 
-        if (status == 404) return res.status(404).send({
-          error,
-          msg: 'Este usuername não possui cadastro...'
-        });
+          } else {
+            // Caso a requisição seja feita na última pagina, não há link de referência
+            const prevLink = links.find(element => element.includes('rel="prev"'));
+            const lastPage = prevLink.split('>;')[0].split('?page=')[1];
+            res.header('X-Last-Page', parseInt(lastPage) + 1);
 
-        return res.status(500).send({
-          error,
-          msg: 'Erro ao tentar efetuar requisição... Tente novamente mais tarde'
-        });
+          }
 
-      })
+        } catch {
+          console.log('[*] Falha ao tentar acessar total de páginas...');
+        }
+      }
+
+      console.log(`[*] Requisição realizada com sucesso (${used}/${limit})`)
+      return res.send(response.data);
+
+
+    } catch (error) {
+      const { status } = error.response;
+      console.log(`[*] Requisição falhou (${status})`);
+
+      if (status == 404) return res.status(404).send({
+        error,
+        msg: 'Este usuername não possui cadastro...'
+      });
+
+      return res.status(500).send({
+        error,
+        msg: 'Erro ao tentar efetuar requisição... Tente novamente mais tarde'
+      });
+
+    }
 
   }
 }
